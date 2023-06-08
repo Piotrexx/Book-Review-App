49874c6e319e984d77eb79a75567a5438f6e6d58
Peter <piotr.lopatecki@gmail.com>
2023-06-08 19:40:24 +0200

    working on admin profile page

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
+from django.template.response import TemplateResponse
+from django.urls import path
+from django.urls.resolvers import URLResolver
 class BookrAdmin(admin.AdminSite):
     site_header = "Bookr admin page"
     logout_template = "admin/logout.html"
+
+    def profile_view(self, request):
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(request, "admin/admin_profile.html", context)
+    def get_urls(self):
+        urls = super().get_urls()
+        url_patterns = [path("admin_profile", self.admin_view(self.profile_view))]
+        return urls + url_patterns
