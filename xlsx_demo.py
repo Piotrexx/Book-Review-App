7e611842d6c228e2118b65225941bed3c0ed74d8
Peter <piotr.lopatecki@gmail.com>
2023-06-21 13:04:22 +0200

    creating xlsx files in python

---
+++ xlsx_demo.py
@@ -0,0 +1,24 @@
+import xlsxwriter
+
+def create_workbook(filename):
+    workbook = xlsxwriter.Workbook(filename)
+    return workbook
+
+def create_worksheet(workbook):
+    worksheet = workbook.add_worksheet()
+    return worksheet
+
+def write_data(worksheet, data):
+    for row in range(len(data)):
+        for col in range(len(data[row])):
+            worksheet.write(row,col,data[row][col])
+
+def close_workbook(workbook):
+    workbook.close()
+
+if __name__ == '__main__':
+    data = [['John Doe', 38], ['Adam Cuvver', 22], ['Stacy Martin', 28], ['Tom Harris', 42]]
+    workbook = create_workbook('sample_workbook.xlsx')
+    worksheet = create_worksheet(workbook)
+    write_data(worksheet, data)
+    close_workbook(workbook)
